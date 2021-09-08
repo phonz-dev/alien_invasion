c71c9c0600eba65c444e129e71af419355ebb784
Alphonzo Escolar <phonz.dev@gmail.com>
2021-09-09 07:04:29 +0800

    Add ship moving left feature

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,7 +27,6 @@ class AlienInvasion:
             self.ship.update()
             self._update_screen()
             
-
     def _check_events(self):
         """Respond to keypresses and mouse events"""
         for event in pygame.event.get():
@@ -36,9 +35,14 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
-                if event.key == K_RIGHT:
+                if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
