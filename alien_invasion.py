19ef9d95e21713aa27b36f0a41f3fd3c128373d9
Alphonzo Escolar <phonz.dev@gmail.com>
2021-09-08 15:31:49 +0800

    Move checking events logic inside _check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,14 +22,18 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
 
             pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == '__main__':
