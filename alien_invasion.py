20e08ac648fd265f2b330cbafc64110bc5620617
Alphonzo Escolar <phonz.dev@gmail.com>
2021-09-08 15:05:37 +0800

    Import ship module and add Ship instance

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -16,6 +17,8 @@ class AlienInvasion:
             (self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -24,6 +27,7 @@ class AlienInvasion:
                     sys.exit()
             
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
 
